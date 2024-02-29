93a80b6f37f1fe0cc9550689d8d131611b88e2da
abl73-public <bolinoarmando@gmail.com>
2024-02-29 02:00:44 +0000

    Code First Student Db

---
+++ CodeFirstStudentDb/CodeFirstStudentDb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudentDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var school = new SchoolContext())
+            {
+                var student = new Student()
+                {
+                    FirstName = "Adam",
+                    LastName = "Clark",
+                    DateOfBirth = new DateTime(2015, 6, 1, 7, 47, 0)
+                };
+                school.Students.Add(student);
+                school.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstStudentDb/CodeFirstStudentDb/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudentDb
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte Photo { get; set; }
+        public double Height { get; set; }
+        public float Weight { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
